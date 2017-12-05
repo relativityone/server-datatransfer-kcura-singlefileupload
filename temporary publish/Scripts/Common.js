7aa20836defada5d2e94c08dfc6adfd7056e3c0f
CRISTHIAN-PC\Cristhian <cristhianc@nserio.com>
2017-12-05 18:21:24 -0500

    DEFECT REL-186313

---
+++ temporary publish/Scripts/Common.js
@@ -0,0 +1,44 @@
+﻿//! add common logic in js here
+function showLoading() {
+    setTimeout(function () {
+        $.blockUI({
+            message: $('#LoadingImage'),
+            css: { padding: 0, margin: 0, left: (($(window).width() / 2) - 120) + 'px', width: '280px', height: '160px', textAlign: 'center', color: 'Black', border: 'none', backgroundColor: 'rgb(234,234,234)', cursor: 'wait' },
+            overlayCSS: { backgroundColor: 'rgb(234,234,234)' },
+            focusInput: false,
+            onBlock: function () {
+
+            }
+        });
+    });
+}   
+function AngularPostOfData($http, url, dataToSend) {
+    var result = new promiseResult();
+    $http.post(location.pathname + url, dataToSend, { headers: { 'AppID': AppID } })
+        .then(function (data) {
+            if (!data.data.Success && !(typeof data.data === 'string'))
+                console.error(data.data.Message);
+            else
+                result.doneCallback(data);
+            result.alwaysCallback();
+        }, function (error) {
+            alert(error.responseText);
+            result.alwaysCallback();
+        });
+    return result;
+}
+function promiseResult() {
+    var self = this;
+    self.doneCallback = function () { }
+    self.alwaysCallback = function () { }
+    self.done = function (callback) {
+        if (typeof callback == 'function')
+            self.doneCallback = callback;
+        return self;
+    }
+    self.always = function (callback) {
+        if (typeof callback == 'function')
+            self.alwaysCallback = callback;
+        return self;
+    }
+}
---
+++ temporary publish/Scripts/SingleFileUpload/SFUApp.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="../angular.js" />
+var SFUAPP = angular.module('sfuapp', []);
