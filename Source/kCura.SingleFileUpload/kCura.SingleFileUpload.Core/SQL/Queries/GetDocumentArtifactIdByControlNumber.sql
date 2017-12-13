2846f310113e2811d595f04011e73f58e270144c
CRISTHIAN-PC\Cristhian <cristhianc@nserio.com>
2017-12-13 12:09:00 -0500

    Fix pull request suggestions

--- Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Core/SQL/Queries/GetDocumentArtifactIdByControlNumber.sql
+++ Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Core/SQL/Queries/GetDocumentArtifactIdByControlNumber.sql
@@ -1,12 +1,17 @@
 ﻿ DECLARE 
 	     @CNField VARCHAR(200)
+
  SELECT 
-	@CNField = REPLACE(DisplayName, ' ', '') 
+	@CNField = AVM.ColumnName 
  FROM 
-	EDDSDBO.Field 
+	EDDSDBO.Field AS F WITH (NOLOCK)
+INNER JOIN 
+	EDDSDBO.ArtifactViewField AS AVM WITH (NOLOCK) 
+	ON 
+	F.ArtifactViewFieldID = AVM.ArtifactViewFieldID
  WHERE 
 	FieldArtifactTypeID = 10 
 	AND 
 	FieldCategoryID = 2
 
-EXEC('SELECT ArtifactID FROM EDDSDBO.Document WHERE '+@CNField+'='''+@ControlNumber+'''')
+EXEC('SELECT ArtifactID FROM EDDSDBO.Document WITH (NOLOCK) WHERE '+@CNField+'='''+@ControlNumber+'''')
