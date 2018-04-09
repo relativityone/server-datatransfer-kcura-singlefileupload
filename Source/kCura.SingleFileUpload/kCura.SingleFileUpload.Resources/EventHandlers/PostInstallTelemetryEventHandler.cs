e2fea2bab102bea5178f747e3b88b3694546f195
Marlon Sanguino <marlons@nserio.com>
2018-04-09 18:07:47 -0500

    Defects: REL-213664 New Features: None Enhancements: None

--- Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Resources/EventHandlers/PostInstallTelemetryEventHandler.cs
+++ Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Resources/EventHandlers/PostInstallTelemetryEventHandler.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace kCura.SingleFileUpload.Resources.EventHandlers
 {
-    [kCura.EventHandler.CustomAttributes.RunTarget(kCura.EventHandler.Helper.RunTargets.Instance)]
+    [kCura.EventHandler.CustomAttributes.RunTarget(kCura.EventHandler.Helper.RunTargets.Workspace)]
     [kCura.EventHandler.CustomAttributes.RunOnce(false)]
     [kCura.EventHandler.CustomAttributes.Description("Single File Upload Post Install Event Handler")]
-    [System.Runtime.InteropServices.Guid("DD7D97E1-8FF7-4E47-A3EF-FB78BC473A9D")]
-    public class SingleFileUploadPostInstallEventHandler : PostInstallEventHandler
+    [System.Runtime.InteropServices.Guid("D94A421D-E7C8-433D-B325-E98998C846BA")]
+    public class SingleFileUploadPostInstallTelemetryEventHandler : PostInstallEventHandler
     {
         IDocumentManager Repository
         {
@@ -27,15 +27,29 @@ namespace kCura.SingleFileUpload.Resources.EventHandlers
         }
         IDocumentManager _repository;
 
+        ITelemetryManager TelemetryRepository
+        {
+            get
+            {
+                if (_telemetryRepository == null)
+                {
+                    _telemetryRepository = new TelemetryManager();
+                }
+                return _telemetryRepository;
+            }
+        }
+        ITelemetryManager _telemetryRepository;
+
         public override Response Execute()
         {
             Response response = new Response();
             RepositoryHelper.ConfigureRepository(this.Helper);
-            using (CacheContextScope d = RepositoryHelper.InitializeRepository(-1))
+            using (CacheContextScope d = RepositoryHelper.InitializeRepository(this.Helper.GetActiveCaseID()))
             {
                 try
                 {
                     Repository.SetCreateInstanceSettings();
+                    executeAsync().Wait();
                     response.Success = true;
                 }
                 catch (Exception e)
@@ -50,5 +64,11 @@ namespace kCura.SingleFileUpload.Resources.EventHandlers
             return response;
         }
 
+        private async Task executeAsync()
+        {
+            await TelemetryRepository.CreateMetricsAsync();
+            await ToggleManager.Instance.SetChangeFileNameAsync(true);
+            await ToggleManager.Instance.SetCheckSFUFieldsAsync(true);
+        }
     }
 }
