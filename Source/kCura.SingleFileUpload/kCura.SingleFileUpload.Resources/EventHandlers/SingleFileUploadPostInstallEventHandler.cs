e4faf855105b17d356ac97de9daf79eb15ae9967
Marlon Sanguino <marlons@nserio.com>
2018-05-08 10:17:25 -0500

    DEFECT: REL-222605

--- Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Resources/EventHandlers/SingleFileUploadPostInstallEventHandler.cs
+++ Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Resources/EventHandlers/SingleFileUploadPostInstallEventHandler.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace kCura.SingleFileUpload.Resources.EventHandlers
 {
     [kCura.EventHandler.CustomAttributes.RunTarget(kCura.EventHandler.Helper.RunTargets.Instance)]
-    [kCura.EventHandler.CustomAttributes.RunOnce(false)]
+    [kCura.EventHandler.CustomAttributes.RunOnce(true)]
     [kCura.EventHandler.CustomAttributes.Description("Single File Upload Post Install Event Handler")]
     [System.Runtime.InteropServices.Guid("D94A421D-E7C8-433D-B325-E98998C846BA")]
     public class SingleFileUploadPostInstallEventHandler : PostInstallEventHandler
