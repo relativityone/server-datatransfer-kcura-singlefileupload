7a55701298f4262df30eb9e87a02619ef09e8fa4
Leonardo Vera <leonardo.vera@relativity.com>
2018-08-13 18:05:18 -0500

    Feature: REL-243027 ENHANCEMENTS: Add success icon

--- Source/kCura.SingleFileUpload/kCura.SingleFileUpload.MVC/Scripts/SingleFileUpload/Controllers/MFUController.js
+++ Source/kCura.SingleFileUpload/kCura.SingleFileUpload.MVC/Scripts/SingleFileUpload/Controllers/MFUController.js
@@ -31,7 +31,9 @@ var MFUController = function ($scope, $http, $compile) {
     vm.uploadFile = UploadFile;
     sessionStorage['____pushNo'] = '';
     var files;
-
+    vm.timelapse;
+    vm.startTime;
+    vm.totalFiles = 0;
     var idCheckTimeout;
 
     function getdH() {
@@ -51,7 +53,10 @@ var MFUController = function ($scope, $http, $compile) {
     } catch (e) { }
 
     function Addfiles(files) {
+        cleanFiles();
         if ((vm.files.length + files.length) > 20) {
+            vm.status = 2;
+            msgLabel.className = "msgDetails";
             msgLabel.innerHTML = "<div class='error' title='You can upload up to 20 files.'><div><img src='/Relativity/CustomPages/1738ceb6-9546-44a7-8b9b-e64c88e47320/Content/Images/Error_Icon.png' /><span>You can upload up to 20 files.</span></div></div>";
         }
         else {
@@ -65,6 +70,10 @@ var MFUController = function ($scope, $http, $compile) {
                 }
             }
         }
+        vm.totalFiles = vm.files.length;
+    }
+    function cleanFiles() {
+        vm.files = vm.files.filter(element => element.status != 3);
     }
     function SubmitFrm() {
         files = document.getElementById("file").files;
@@ -125,9 +134,18 @@ var MFUController = function ($scope, $http, $compile) {
     }
 
     function UploadFiles() {
-        vm.status = 1;
-        vm.fileIndex = 0;
-        UploadFile(0)
+        cleanFiles();
+        vm.totalFiles = vm.files.length;
+        if (vm.totalFiles > 0) {
+            vm.status = 0;
+            calculateTime();
+            vm.status = 1;
+            vm.fileIndex = 0;
+            UploadFile(0);
+        }
+        else {
+            vm.status = 0;
+        }
     }
 
     function UploadFile(fileIndex, retry) {
@@ -136,6 +154,10 @@ var MFUController = function ($scope, $http, $compile) {
         var file = vm.files[fileIndex];
         if (retry) {
             vm.status = 0;
+            calculateTime();
+            vm.totalFiles = 1;
+            vm.status = 1;
+            vm.fileIndex = 0;
         }
         file.status = 1;
         if (vm.errorID == 0) {
@@ -152,7 +174,7 @@ var MFUController = function ($scope, $http, $compile) {
                 var resultString = sessionStorage['____pushNo'] || '';
                 sessionStorage['____pushNo'] = '';
                 checkUpload(file, resultString);
-                if ((fileIndex < vm.files.length - 1) && !retry) {
+                if ((fileIndex < vm.totalFiles - 1) && !retry) {
                     fileIndex++;
                     UploadFile(fileIndex);
                 }
@@ -229,13 +251,13 @@ var MFUController = function ($scope, $http, $compile) {
     }
     function Uploading() {
         var elem = document.getElementById("progressBar");
-        var porcent = 100 / vm.files.length;
+        var porcent = 100 / vm.totalFiles;
         var width = porcent * vm.fileIndex;
         setTimeout(function () {
             elem.style.width = width + '%';
             if ((width) >= 100) {
                 $scope.$apply(function () {
-                    vm.status = 0;
+                    vm.status = 3;
                 });
                 elem.style.width = '1%';
             }
@@ -284,6 +306,36 @@ var MFUController = function ($scope, $http, $compile) {
         if (document.getElementById('fdv') != null && document.getElementById('fdv').getAttribute('value') == 'true')
             did = GetQueryStringValueByName(window.parent.location.search, "ArtifactID");
         return did;
+    }
+    function calculateTime(apply) {
+        if (vm.status == 0) {
+            vm.startTime = new Date();
+        }
+        var today = new Date();
+        var diference = (today.getTime() - vm.startTime.getTime()) / 1000;
+        var s = Math.floor(diference % 60);
+        diference = diference / 60;
+        var m = Math.floor(diference % 60);
+        diference = diference / 60;
+        var h = Math.floor(diference % 24);
+        m = checkTime(m);
+        s = checkTime(s);
+        h = checkTime(h);
+        if (apply) {
+            $scope.$apply(function () {
+                vm.timelapse = h + ":" + m + ":" + s;
+            });
+
+        } else {
+            vm.timelapse = h + ":" + m + ":" + s;
+        }
+        if (vm.status != 3) {
+            var t = setTimeout(function () { calculateTime(true) }, 500);
+        }
+    }
+    function checkTime(i) {
+        if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+        return i;
     }
 }
 MFUController.$inject = ['$scope', '$http', '$compile'];
