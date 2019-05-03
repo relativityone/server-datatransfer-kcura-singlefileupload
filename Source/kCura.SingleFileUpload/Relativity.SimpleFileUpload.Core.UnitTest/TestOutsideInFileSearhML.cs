975c0dfb59734646e4f642e65c781927542d2f32
Jeison Triana <jeison.triana@relativity.com>
2019-05-02 19:10:10 -0500

    added new unit Test to the project.

---
+++ Source/kCura.SingleFileUpload/Relativity.SimpleFileUpload.Core.UnitTest/TestOutsideInFileSearhML.cs
@@ -0,0 +1,43 @@
+﻿using kCura.SingleFileUpload.Core.Entities;
+using kCura.SingleFileUpload.Core.Managers.Implementation;
+using NUnit.Framework;
+using System.IO;
+
+
+namespace Relativity.SimpleFileUpload.Core.UnitTest
+{
+
+	[TestFixture]
+	public class TestOutsideInFileSearhML
+	{
+		[OneTimeSetUp]
+		public void OnStepUp()
+		{
+			
+		}
+
+		[Test]
+		public void TestFile()
+		{
+			string executableLocation = TestContext.CurrentContext.TestDirectory;
+			string fileName = "CTRL0187228.msg";
+			string fileLocation = Path.Combine(executableLocation, "..\\", "..\\", "Resources", fileName);
+			using (FileStream fileStream = new FileStream(fileLocation, FileMode.Open, FileAccess.Read))
+			{
+
+				Stream stream = fileStream;
+				var native = new byte[stream.Length];
+				stream.Read(native, 0, checked((int)stream.Length));
+
+				SearchExportManager searchExportManager = new SearchExportManager();
+				ExportedMetadata transientMetadata = new ExportedMetadata();
+
+				searchExportManager.ConfigureOutsideIn();
+				transientMetadata = searchExportManager.ExportToSearchML(fileName, native, () => OutsideIn.OutsideIn.NewLocalExporter());
+			}
+
+
+
+		}
+	}
+}
