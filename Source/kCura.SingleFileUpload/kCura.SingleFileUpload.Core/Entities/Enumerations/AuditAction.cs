26cebee41aaf1e315e9fba77e7ea2645ad634f5a
Leonardo Vera <leonardo.vera@relativity.com>
2019-07-09 14:09:05 -0500

    REL-336402: update importJob to not create audit records with the system user and add audits records with the current user

--- Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Core/Entities/Enumerations/AuditAction.cs
+++ Source/kCura.SingleFileUpload/kCura.SingleFileUpload.Core/Entities/Enumerations/AuditAction.cs
@@ -6,6 +6,9 @@
         Update = 3,
         File_Upload = 52,
         File_Download = 53,
-        Images_Deleted = 14
-    }
+		Images_Deleted = 14,
+		Update_Import = 47,
+		Native_Created = 16,
+		Native_Deleted = 17
+	}
 }
