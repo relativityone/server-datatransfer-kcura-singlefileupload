efa94b0d595ae8fa406ba702abe8021c2dc1104f
Tomasz Pęczek <tomasz.peczek@relativity.com>
2020-11-24 13:38:39 +0100

    REL-496543] UI native upload GOLD flow test

--- Source/kCura.SingleFileUpload/Relativity.SimpleFileUpload.UiTests/NativeFileUploadTests.cs
+++ Source/kCura.SingleFileUpload/Relativity.SimpleFileUpload.UiTests/NativeFileUploadTests.cs
@@ -1,5 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Atata;
+using System.IO;
+using NUnit.Framework;
+using Relativity.Testing.Framework.Web;
 using Relativity.Testing.Identification;
+using kCura.SingleFileUpload.Core.Tests.Constants;
 
 namespace Relativity.SimpleFileUpload.UiTests
 {
@@ -11,5 +15,18 @@ namespace Relativity.SimpleFileUpload.UiTests
 		public NativeFileUploadTests()
 			: base(nameof(NativeFileUploadTests))
 		{ }
+
+		[IdentifiedTest("bf01c1a8-cf93-47e3-8c57-a2e77ea5725a")]
+		public void UploadNativeFile_GoldFlow()
+		{
+			// Arrange
+			DocumentListPage documentListPage = Being.On<DocumentListPage>(WorkspaceId);
+
+			// Act
+			documentListPage = documentListPage.NewDocument.ClickAndGo().Upload(Path.GetFullPath(TestsConstants._FILE_LOCATION));
+
+			// Assert
+			documentListPage.Documents.Rows[x => x.ControlNumber == Path.GetFileNameWithoutExtension(TestsConstants._FILE_NAME)].Should.BeVisible();
+		}
 	}
 }
