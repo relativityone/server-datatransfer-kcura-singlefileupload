2369e5ef323026dc71c0f05d1e4865432c6924d2
Camilo Molina <camilo.molina@relativity.com>
2021-07-01 12:13:12 -0500

    Object manager implementation in functional tests project updated

--- Source/kCura.SingleFileUpload/Relativity.SimpleFileUpload.FunctionalTests/CI/SimpleFileUploadTestsTemplate.cs
+++ Source/kCura.SingleFileUpload/Relativity.SimpleFileUpload.FunctionalTests/CI/SimpleFileUploadTestsTemplate.cs
@@ -22,6 +22,7 @@ namespace Relativity.SimpleFileUpload.FunctionalTests.CI
 		private readonly string _workspaceTemplateName;
 
 		private readonly IWorkspaceService _workspaceService;
+		private readonly IObjectManager _objectManager;
 
 		public int WorkspaceId { get; private set; }
 		public int ImagingProfileId { get; private set; }
@@ -32,6 +33,7 @@ namespace Relativity.SimpleFileUpload.FunctionalTests.CI
 			_workspaceTemplateName = workspaceTemplateName;
 
 			_workspaceService = RelativityFacade.Instance.Resolve<IWorkspaceService>();
+			_objectManager = RelativityFacade.Instance.Resolve<IObjectManager>();
 		}
 
 		protected override void OnSetUpFixture()
@@ -68,36 +70,28 @@ namespace Relativity.SimpleFileUpload.FunctionalTests.CI
 
 		private async Task<int> GetDefaultProcessingProfileAsync(int workspaceId)
 		{
-			using (var objectManagerProxy = RelativityFacade.Instance.Resolve<ApiComponent>().ServiceFactory.GetServiceProxy<IObjectManager>())
-			{
 
-				IEnumerable<FieldRef> queryFields = new List<FieldRef>() {
+			IEnumerable<FieldRef> queryFields = new List<FieldRef>() {
 					new FieldRef() { Name = "ArtifactID" },
 				};
 
-				string condition = $"'Name' == 'Imaging Profile'";
+			string condition = $"'Name' == 'Imaging Profile'";
 
-				QueryRequest queryRequest = BuildQueryRequest(queryFields, (int)ArtifactType.ObjectType, condition);
-				QueryResult queryResult = await BuildQueryAsync(objectManagerProxy, workspaceId, queryRequest, 0, 0);
+			QueryRequest objTypeQueryRequest = BuildQueryRequest(queryFields, (int)ArtifactType.ObjectType, condition);
+			QueryResult objTypeQueryResult = await _objectManager.QueryAsync(workspaceId, objTypeQueryRequest, 0, 0);
 
-				int objectTypeArtifactId = queryResult.Objects.FirstOrDefault().ArtifactID;
+			int objectTypeArtifactId = objTypeQueryResult.Objects.FirstOrDefault().ArtifactID;
 
-				queryFields = new List<FieldRef>() {
+			queryFields = new List<FieldRef>() {
 					new FieldRef() { Name = "ArtifactID" },
 				};
 
-				condition = $"'Name' == 'Basic Default'";
+			condition = $"'Name' == 'Basic Default'";
 
-				queryRequest = BuildQueryRequest(queryFields, objectTypeArtifactId, condition);
-				queryResult = await BuildQueryAsync(objectManagerProxy, workspaceId, queryRequest, 0, 0);
+			QueryRequest queryRequest = BuildQueryRequest(queryFields, objectTypeArtifactId, condition);
+			QueryResult queryResult = await _objectManager.QueryAsync(workspaceId, queryRequest, 0, 0);
 
-				return queryResult.Objects.FirstOrDefault().ArtifactID;
-			}
-		}
-
-		private Task<QueryResult> BuildQueryAsync(IObjectManager objectManagerProxy, int workspaceID, QueryRequest queryRequest, int start, int length)
-		{
-			return objectManagerProxy.QueryAsync(workspaceID, queryRequest, start, length);
+			return queryResult.Objects.FirstOrDefault().ArtifactID;
 		}
 
 		private QueryRequest BuildQueryRequest(IEnumerable<FieldRef> queryFields, int typeId, string condition)
