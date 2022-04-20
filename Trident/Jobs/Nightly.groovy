9ed43a2eb65b4024bb018c799f3e8782ad371769
Kamil Makarowski <kamil.makarowski@relativity.com>
2022-04-20 09:30:49 +0200

    REL-672353 Update SutTemplate to aio-tigerlily-1

--- Trident/Jobs/Nightly.groovy
+++ Trident/Jobs/Nightly.groovy
@@ -2,7 +2,7 @@
 
 jobWithSut {
     slackChannel = "sfu_trident"
-    sutTemplate = "aio-prairiesmoke-2"
+    sutTemplate = "aio-tigerlily-1"
 	relativityBranch = "develop"
     jobScript = "Trident/Scripts/nightly-job.ps1"
     cron = "0 3 * * *"
