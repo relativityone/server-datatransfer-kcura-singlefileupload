087e50266e1664ba544debc20fe0b92457a95b45
Artur Jaśkiewicz <artur.jaskiewicz@relativity.com>
2023-01-20 16:02:20 +0100

    REL-721151 Disabled MVC response header

--- Source/kCura.SingleFileUpload/kCura.SingleFileUpload.MVC/Global.asax.cs
+++ Source/kCura.SingleFileUpload/kCura.SingleFileUpload.MVC/Global.asax.cs
@@ -6,20 +6,22 @@ using kCura.SingleFileUpload.Core.Relativity;
 
 namespace kCura.SingleFileUpload.MVC
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
-	public class MvcApplication : System.Web.HttpApplication
-	{
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
 
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			BundleConfig.RegisterBundles();
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-			RepositoryHelper.ConfigureRepository(ConnectionHelper.Helper());
-		}
-	}
+            RepositoryHelper.ConfigureRepository(ConnectionHelper.Helper());
+
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+    }
 }
