fe44bd14531cb9dd3bff67d08bff0f968508ab53
Ravi Konnur <ravi.konnur@relativity.com>
2023-11-22 17:30:59 +0530

    Modified files as per migration SOP

--- Trident/Scripts/nightly-job.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.SYNOPSIS
-This script will be used by nightly pipeline to complie and run RelativitySync tests
-#>
-
-$TaskRunner = Resolve-Path -Path build.ps1
-
-&($TaskRunner) Compile, Package, FunctionalTest -Configuration Release
