b1cc8627159d3116587d49244e0360d83af82210
Ravi Konnur <ravi.konnur@relativity.com>
2023-11-22 19:51:56 +0530

    Modified files as per migration SOP

--- Trident/Jobs/Nightly.groovy
+++
@@ -1,9 +0,0 @@
-@Library('ProjectMayhem@v1') _
-
-jobWithSut {
-    slackChannel = "ci-sfu"
-    sutTemplate = "aio-whitesedge-latest"
-	//relativityBranch = "develop"
-    jobScript = "Trident/Scripts/nightly-job.ps1"
-    cron = "0 3 * * *"
-}
